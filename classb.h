b3c23e0f79d6ca52a62d70e1fd32c6477456e719
AlonyDR <44353961+AlonyDR@users.noreply.github.com>
2018-10-30 19:03:12 +0700

    Add files via upload

--- classb.h
+++ classb.h
@@ -1,48 +1,73 @@
 #include "stdafx.h"
+#include <iostream>
 #include <windows.h>
+#include <string>
+
+using namespace std;
 
 class bdbd
 {
 private:
-	int DIV; 
+	int MB; 
 	int WIDTH;
 	MEMORYSTATUSEX statex;
+	int inf[8];
+	static DWORD WINAPI S_Run(LPVOID p)
+		{
+		  return ((bdbd*)p)->Run();
+		}
+		DWORD Run()
+		{
+			tri();
+			return 0;
+		}
 public:
 	bdbd()
 	{
-		DIV = 1048576;
-		WIDTH = 7;
+		MB = 1048576;
 	}
 	void one()
 	{
 		  statex.dwLength = sizeof (statex);
-
 		  GlobalMemoryStatusEx (&statex);
-		  _tprintf (TEXT("There is  %*ld percent of memory in use.\n"),WIDTH, statex.dwMemoryLoad);
-		  _tprintf (TEXT("There are %*I64d total Mbytes of physical memory.\n"),WIDTH,statex.ullTotalPhys/DIV);
-		  _tprintf (TEXT("There are %*I64d free Mbytes of physical memory.\n"),WIDTH, statex.ullAvailPhys/DIV);
-		  _tprintf (TEXT("There are %*I64d total Mbytes of paging file.\n"),WIDTH, statex.ullTotalPageFile/DIV);
-		  _tprintf (TEXT("There are %*I64d free Mbytes of paging file.\n"),WIDTH, statex.ullAvailPageFile/DIV);
-		  _tprintf (TEXT("There are %*I64d total Mbytes of virtual memory.\n"),WIDTH, statex.ullTotalVirtual/DIV);
-		  _tprintf (TEXT("There are %*I64d free Mbytes of virtual memory.\n"),WIDTH, statex.ullAvailVirtual/DIV);
-		  _tprintf (TEXT("There are %*I64d free Mbytes of extended memory.\n"),WIDTH, statex.ullAvailExtendedVirtual/DIV);
+
+		  inf[0]=statex.dwMemoryLoad;
+		  inf[1]=statex.ullTotalPhys/MB;
+		  inf[2]=statex.ullAvailPhys/MB;
+		  inf[3]=statex.ullTotalPageFile/MB;
+		  inf[4]=statex.ullAvailPageFile/MB;
+		  inf[5]=statex.ullTotalVirtual/MB;
+		  inf[6]=statex.ullAvailVirtual/MB;
+		  inf[7]=statex.ullAvailExtendedVirtual/MB;
 	}
 	void dwa()
 	{
-		  for(int i=0;i<10;i++)
+		wcout<<"There is  percent of memory in use "<<inf[0]<<endl;
+		wcout<<"There are total Mbytes of physical memory "<<inf[1]<<endl;
+		wcout<<"There are free Mbytes of physical memory "<<inf[2]<<endl;
+		wcout<<"There are total Mbytes of paging file "<<inf[3]<<endl;
+		wcout<<"There are free Mbytes of paging file "<<inf[4]<<endl;
+		wcout<<"There are total Mbytes of virtual memory "<<inf[5]<<endl;
+		wcout<<"There are free Mbytes of virtual memory "<<inf[6]<<endl;
+		wcout<<"There are free Mbytes of extended memory "<<inf[7]<<endl;
+	}
+	void tri()
+	{
+		  for(;;)
 		  {
 			  system("cls");
-				GlobalMemoryStatusEx (&statex);
-			  _tprintf (TEXT("There is  %*ld percent of memory in use.\n"),WIDTH, statex.dwMemoryLoad);
-			  _tprintf (TEXT("There are %*I64d total Mbytes of physical memory.\n"),WIDTH,statex.ullTotalPhys/DIV);
-			  _tprintf (TEXT("There are %*I64d free Mbytes of physical memory.\n"),WIDTH, statex.ullAvailPhys/DIV);
-			  _tprintf (TEXT("There are %*I64d total Mbytes of paging file.\n"),WIDTH, statex.ullTotalPageFile/DIV);
-			  _tprintf (TEXT("There are %*I64d free Mbytes of paging file.\n"),WIDTH, statex.ullAvailPageFile/DIV);
-			  _tprintf (TEXT("There are %*I64d total Mbytes of virtual memory.\n"),WIDTH, statex.ullTotalVirtual/DIV);
-			  _tprintf (TEXT("There are %*I64d free Mbytes of virtual memory.\n"),WIDTH, statex.ullAvailVirtual/DIV);
-			  _tprintf (TEXT("There are %*I64d free Mbytes of extended memory.\n"),WIDTH, statex.ullAvailExtendedVirtual/DIV);
-			  Sleep(2000);
+			  one();
+			  dwa();
+			  Sleep(1000);
 		  }
+	}
+	void chet()
+	{
+		DWORD id = 0;
+		HANDLE h = CreateThread(NULL, 0, S_Run, this, 0, &id);
+		WaitForSingleObject(h,INFINITE);
+		CloseHandle(h);
+
 	}
 
 
--- dwa.cpp
+++ dwa.cpp
@@ -1,6 +1,5 @@
 // dwa.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
 #include <windows.h>
 #include "classb.h"
@@ -10,10 +9,11 @@
 int main()
 {
 	 bdbd bd;
-	 bd.one();
-	 bd.dwa();
+	 //bd.one();
+	 //bd.dwa();
+	 //bd.tri();
+	 bd.chet();
 
 	system("pause");
 	return 0;
 }
-
